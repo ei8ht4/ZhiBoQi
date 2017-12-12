1b0b2d5296d89c59952682954cafe987b66faba6
hehe <hehe@hehes-iMac.local>
2017-12-12 20:18:21 +0800

    Initial Commit

---
+++ ZhiBoQi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhiBoQi
+//
+//  Created by hehe on 2017/12/12.
+//  Copyright © 2017年 hehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZhiBoQi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhiBoQi
+//
+//  Created by hehe on 2017/12/12.
+//  Copyright © 2017年 hehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
